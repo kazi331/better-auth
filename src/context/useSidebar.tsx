ed45a6b57d253fd594951e6055c3e89a8dcbf49b
kazi331 <kazisharifulislam52@gmail.com>
2025-09-21 01:26:40 +0600

    sidebar context improved and encapsulated

--- src/context/useSidebar.tsx
+++ src/context/useSidebar.tsx
@@ -1,9 +1,13 @@
-import { createContext, useContext } from "react";
+import { createContext, ReactNode, useContext, useState } from "react";
 
 const sidebarContext = createContext({ isOpen: true, toggleSidebar: () => { } });
-export const SidebarProvider = sidebarContext.Provider;
 
-export const useSidebar = () => {
-    const sidebarState = useContext(sidebarContext);
-    return sidebarState
-};
+
+export const SidebarProvider = ({ children }: { children: ReactNode }) => {
+    const [isOpen, setIsOpen] = useState(true);
+    const toggleSidebar = () => setIsOpen(prev => !prev);
+
+    return <sidebarContext.Provider value={{ isOpen, toggleSidebar }}>{children}</sidebarContext.Provider>
+}
+
+export const useSidebar = () => useContext(sidebarContext)
