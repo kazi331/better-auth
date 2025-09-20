04c8a6f02029c196b7cdfc8a0152e90bc762df64
kazi331 <kazisharifulislam52@gmail.com>
2025-09-21 01:33:40 +0600

    context improved

--- src/context/sidebarContext.tsx
+++ src/context/sidebarContext.tsx
@@ -1,10 +1,15 @@
 import { createContext, ReactNode, useContext, useState } from "react";
 
-const sidebarContext = createContext({ isOpen: true, toggleSidebar: () => { } });
+const initialState = {
+    isOpen: true,
+    toggleSidebar: () => { }
+}
+
+const sidebarContext = createContext(initialState);
 
 
 export const SidebarProvider = ({ children }: { children: ReactNode }) => {
-    const [isOpen, setIsOpen] = useState(true);
+    const [isOpen, setIsOpen] = useState(initialState.isOpen);
     const toggleSidebar = () => setIsOpen(prev => !prev);
 
     return <sidebarContext.Provider value={{ isOpen, toggleSidebar }}>{children}</sidebarContext.Provider>
